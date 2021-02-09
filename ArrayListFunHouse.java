9c9c9c4a08fc0e3f05e2fc55882f1a1984fb7b21
Kamden Wright <600022173@fjuhsd.org>
2021-02-09 23:03:30 +0000

    Added a method to check for compostite numbers

--- ArrayListFunHouse.java
+++ ArrayListFunHouse.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Kamden Wright
 
 import java.util.Scanner;
 import java.util.List;
@@ -12,10 +12,25 @@ public class ArrayListFunHouse
 {
  public static ArrayList<Integer> getListOfFactors(int number)
  {
-  return null;
+ ArrayList<Integer>numbers = new ArrayList<Integer>();
+
+  for(int i=2; i<number; i++) {
+    if (number % i==0)
+    numbers.add(i);
+  }
+
+return numbers;
+
  }
  
  public static void keepOnlyCompositeNumbers( List<Integer> nums )
  {
+   for(int i = nums.size()-1; i >= 0; i--){
+     if(getListOfFactors(nums.get(i)).size()==0){
+       nums.remove(i);
+     }
+
+   }
+  
  }
 }
